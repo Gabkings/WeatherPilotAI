34308401c426d0fc7e15ba68fbceb38496bf1419
gabkings <gabworks51@gmail.com>
2026-06-05 08:01:51 +0300

    quick fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.weather-ai.co/v1",
     headers: {
-        Authorization: `Bearer ${
-            import.meta.env.VITE_WEATHER_AI_KEY
-        }`,
+        Authorization: `Bearer wai_2ee780.00a6e0d8c3167d93a3e70287564a9beb30b9d07a99d75ede`,
     },
 });
