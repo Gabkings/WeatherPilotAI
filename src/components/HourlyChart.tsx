5e72e73faed150680d9d9d87df38c242e80371ac
gabkings <gabworks51@gmail.com>
2026-06-05 05:45:00 +0300

    Adding hourly chart to display line chart for 24 hours temperature

--- src/components/HourlyChart.tsx
+++ src/components/HourlyChart.tsx
@@ -1,7 +1,29 @@
 
-const HourlyChart = () => {
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
+
+
+
+
+const HourlyChart = ({data} :any) => {
     return (
-        <div>HourlyChart</div>
-    )
+        <ResponsiveContainer
+            width="100%"
+            height={300}
+        >
+            <LineChart data={data}>
+                <XAxis dataKey="hour" />
+                <YAxis />
+                <Tooltip />
+                <Line dataKey="temperature" />
+            </LineChart>
+        </ResponsiveContainer>
+    );
 }
 export default HourlyChart
